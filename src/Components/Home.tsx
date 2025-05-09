83653980c1170ef6e067c3f7f227602e43ea8277
Vijaya Suriyan V <75218921+VijayasuriyanV@users.noreply.github.com>
2025-05-09 17:36:06 +0530

    fixed import in home

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -2,7 +2,7 @@ import {useEffect} from "react";
 import {motion} from "framer-motion";
 import Lenis from "@studio-freight/lenis";
 import Navbar from "./Common/Navbar";
-import {Link, useNavigate} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 
 function Home() {
   const navigate = useNavigate();
