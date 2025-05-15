896f5a42edd0bbd1a54391d3df7362d6db21ad27
Vijaya Suriyan V <75218921+VijayasuriyanV@users.noreply.github.com>
2025-05-15 10:55:55 +0530

    Refactor AppRouter to directly use Piechart component and remove unnecessary lazy loading for Signin and Signup

--- src/Router/AppRouter.tsx
+++ src/Router/AppRouter.tsx
@@ -6,13 +6,13 @@ import Layout from "../Components/Layout/Layout";
 import Home from "../Components/Home";
 import PrivateRoute from "../Auth/PrivateRoute";
 import {PropagateLoader} from "react-spinners";
+import Piechart from "../Components/Piechart";
 
 // Lazy loaded components
 const CurrencyConverter = lazy(() => import("../Components/CurrencyConverter"));
 const Signin = lazy(() => import("../Components/Signin"));
 const Signup = lazy(() => import("../Components/Signup"));
 const ErrorPage = lazy(() => import("../Pages/ErrorPage/ErrorPage"));
-const PieChart = lazy(() => import("recharts").then((mod) => ({default: mod.PieChart}))); // PieChart from recharts
 
 const Loading = (
   <div className="flex items-center justify-center min-h-screen">
@@ -26,21 +26,12 @@ const AppRouter = createBrowserRouter([
     element: <Layout />,
     children: [
       {index: true, element: <Home />},
-      {
-        path: "signin",
-        element: <Signin />,
-      },
-      {
-        path: "signup",
-        element: <Signup />,
-      },
+
       {
         path: "dashboard",
         element: (
           <PrivateRoute>
-            <Suspense fallback={Loading}>
-              <PieChart />
-            </Suspense>
+            <Piechart />
           </PrivateRoute>
         ),
       },
